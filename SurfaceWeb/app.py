e8ba2f14d5303a60cd2241f29dfed19b2062ba2f
Vicky Kumar <imvickykumar999@gmail.com>
2024-07-16 21:30:16 +0530

    button css

---
+++ SurfaceWeb/app.py
@@ -0,0 +1,30 @@
+
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_image():
+    if request.method == 'POST':
+        if 'image' in request.files:
+            image = request.files['image']
+            if image.filename != '':
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+                image.save(filepath)
+                return redirect(url_for('upload_image'))
+    return render_template('index.html')
+
+@app.route('/uploads')
+def list_uploads():
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('uploads.html', files=files)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
